5cf2ec6c3d554db10e98e292f42654dfb21ecf5f
alexkv94 <101505335+alexkv94@users.noreply.github.com>
2023-03-22 09:05:08 +0000

    modified models.py tables

--- temperature_data/models.py
+++ temperature_data/models.py
@@ -3,24 +3,24 @@ from django.db import models
 # Create your models here.
 class Country_data(models.Model):
     date = models.DateField()
-    average_temperature = models.CharField(max_length = 10)
-    average_temperature_uncertainty = models.CharField(max_length = 10)
+    average_temperature = models.FloatField()
+    average_temperature_uncertainty = models.FloatField()
     country = models.CharField(max_length = 100)
 
     def __str__(self):
-        return f"{self.date} {self.country}, {self.average_temperature}"
+        return f'{self.date},{self.average_temperature},{self.average_temperature_uncertainty},{self.country}'
 
-class City_data(models.Model):
+class Major_city_data(models.Model):
     date = models.DateField()
-    average_temperature = models.CharField(max_length = 10)
-    average_temperature_uncertainty = models.CharField(max_length = 10)
-    country = models.CharField(max_length = 100)
-    city = models.CharField(max_length = 100)
+    average_temperature = models.FloatField()
+    average_temperature_uncertainty = models.FloatField()
+    major_city = models.CharField(max_length = 100)
     latitude = models.CharField(max_length = 100)
     longitude = models.CharField(max_length = 100)
 
     def __str__(self):
-        return f"{self.date}, {self.city}, {self.country}, {self.average_temperature}"
+        return f'{self.date},{self.average_temperature},{self.average_temperature_uncertainty},{self.major_city},{self.latitude},{self.longitude}'
+
 
 class State_data(models.Model):
     date = models.DateField()
@@ -31,6 +31,28 @@ class State_data(models.Model):
 
     def __str__(self):
         return f"{self.date}, {self.state}, {self.country}, {self.average_temperature}"
+
+class Countries(models.Model):
+    id = models.AutoField(primary_key = True)
+    country = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return f'{self.id},{self.country}'
+
+class Major_cities(models.Model):
+    id = models.AutoField(primary_key = True)
+    major_city = models.CharField(max_length = 100)
+    country_id = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.id},{self.major_city},{self.country_id}'
+
+class States(models.Model):
+    id = models.AutoField(primary_key = True)
+    state = models.CharField(max_length = 100)
+    country_id = models.IntegerField()
+
+
     
 
 
