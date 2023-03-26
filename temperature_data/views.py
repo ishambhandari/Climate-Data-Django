5c24a032f9ce29ae222a71209a9af34a64e59f44
SIMI MATHAI SIM - <simimathaisim.22@abdn.ac.uk>
2023-03-26 16:02:26 +0000

    Display of country state and city

--- temperature_data/views.py
+++ temperature_data/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from .models import Country_data,State_data,City_data
+from .models import Country,State,City,Country_data,State_data,City_data
 
 # Create your views here.
 def index(request):
     return render(request, 'temperature_data/index.html')
 
 def display_data(request):
-    countries = Country_data.objects.all()[:5]          # loads only first five entries for country
-    states = State_data.objects.all()[:5]
-    cities = City_data.objects.all()[:5]
+    countries = Country.objects.all()[:5]          # loads only first five entries for country
+    states = State.objects.all()[:5]
+    cities = City.objects.all()[:5]
     context = {
         'countries':countries,
         'states': states,
@@ -17,14 +17,14 @@ def display_data(request):
     return render(request, 'temperature_data/display_data.html',context)
 
 def load_countrydata(request):
-    countries = Country_data.objects.all()          # loads complete entries for country
+    countries = Country.objects.all()          # loads complete entries for country
     return render(request, 'temperature_data/load_countrydata.html',{'countries':countries})
 
 def load_statedata(request):
-    states = State_data.objects.all()
+    states = State.objects.all()
     return render(request,'temperature_data/load_statedata.html',{'states':states})
 
 def load_citiesdata(request):
-    cities = City_data.objects.all()
+    cities = City.objects.all()
     return render(request,'temperature_data/load_citiesdata.html',{'cities':cities})
     
