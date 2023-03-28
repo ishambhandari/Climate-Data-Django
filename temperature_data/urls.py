07b708a639bc95c1c73de97c164a1915f578af89
SIMI MATHAI SIM - <simimathaisim.22@abdn.ac.uk>
2023-03-28 14:13:57 +0000

    Chart templates added

--- temperature_data/urls.py
+++ temperature_data/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     path('', views.index, name ='home'),
     path('display_data', views.display_data, name ='display_data'),
     path('countries',views.load_countrydata, name='load_countrydata'),
+    path('country_list/<country_name>',views.country_details, name='country_details'),
     path('states',views.load_statedata, name='load_statedata'),
     path('cities',views.load_citiesdata, name='load_citiesdata'),
+    path('visualize/',views.visualize, name='visualize'),
+    path('countrychart/',views.country_chart, name='country_chart'),
+    path('statechart/',views.state_chart, name='state_chart'),
+    path('citychart/',views.city_chart, name='city_chart'),
 ]
--- temperature_data/views.py
+++ temperature_data/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Country,State,City,Country_data,State_data,City_data
 
 # Create your views here.
@@ -16,9 +16,64 @@ def display_data(request):
     }
     return render(request, 'temperature_data/display_data.html',context)
 
-def load_countrydata(request):
+def load_countrydata(request):  
     countries = Country.objects.all()          # loads complete entries for country
     return render(request, 'temperature_data/load_countrydata.html',{'countries':countries})
+
+def search_country(request):
+    country_search = request.GET.get('search')
+    countries = Country.objects.all()   
+    if country_search:
+        countries = countries.filter(country__icontains=country_search)
+    else:
+        countries = None
+    return render(request, 'temperature_data/search_country.html',{'countries':countries,'country_search':country_search})
+
+def country_details(request,country_name):
+    country_name = Country.objects.filter(country=country_name).first()
+    print('this is country name', country_name)
+    country_list = Country_data.objects.all()
+    context = {
+        'country_name' : country_name,
+        'country_list' : country_list,
+    }
+    return render(request,'temperature_data/country_details.html',context) 
+
+def visualize(request):
+    if requested.method == 'POST':
+        selected_option = request.POST.get('dropdown')
+        if selected_option == 'option1':
+            return render(request,'temperature_data/country_chart.html')
+        elif selected_option == 'option2':
+            return render(request,'temperature_data/state_chart.html')
+        elif selected_option == 'option3':
+            return render(request,'temperature_data/city_chart.html')
+        else:
+    return render(request,'temperature_data/visualize.html')
+
+def country_chart(request):
+    countries = Country_data.objects.all()
+    date = [c.date for c in countries]
+    average_temperature = [c.average_temperature for c in countries]
+    data = {'labels':date, 'data':average_temperature}
+    chart = json.dumps(data)
+    return render(request,'temperature_data/country_chart.html',{'chart':chart})
+
+def state_chart(request):
+    states = State_data.objects.all()
+    date = [s.date for s in states]
+    average_temperature = [s.average_temperature for s in states]
+    data = {'labels':date, 'data':average_temperature}
+    chart = json.dumps(data)
+    return render(request,'temperature_data/state_chart.html',{'chart':chart})
+
+def city_chart(request):
+    cities = City_data.objects.all()
+    date = [ci.date for ci in cities]
+    average_temperature = [ci.average_temperature for ci in cities]
+    data = {'labels':date, 'data':average_temperature}
+    chart = json.dumps(data)
+    return render(request,'temperature_data/city_chart.html',{'chart':chart})
 
 def load_statedata(request):
     states = State.objects.all()
